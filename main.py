9919d2804cc07b42b78566cdc5f04e83a2bc7e9d
Asma <136265381+uchuucho@users.noreply.github.com>
2025-08-17 18:34:55 +0200

    Adding Player class

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     # Initliazing pygame
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     # Getting new GUI 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     # Game loop
     while(True):
         # Checking if user has closed window
@@ -16,10 +21,15 @@ def main():
                 return
         # Filling the screen with black
         screen.fill((0,0,0))
+        # Drawing the player
+        for d in drawable:
+            d.draw(screen)
+        updatable.update(dt)
         # Refreshing the screen
         pygame.display.flip()
         #restricting fps to 60
         dt = (clock.tick(60)/1000)
+        
 
 
 
