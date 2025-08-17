654e80c229ba591e61b417dc59d4be414db11b10
Asma <136265381+uchuucho@users.noreply.github.com>
2025-08-17 20:35:21 +0200

    Added Astroid and AstroidField classes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     # Initliazing pygame
@@ -9,10 +11,14 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     # Getting new GUI 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid = AsteroidField()
     # Game loop
     while(True):
         # Checking if user has closed window
